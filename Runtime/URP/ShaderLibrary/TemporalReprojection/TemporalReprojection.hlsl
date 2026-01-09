49cae2f076963cc3473caf35e2e6904b1bf7c0e2
Math <matthias-merkl@gmx.de>
2026-01-09 22:30:27 +0100

    Initial commit

---
+++ Runtime/URP/ShaderLibrary/TemporalReprojection/TemporalReprojection.hlsl
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include "Packages/eu.rayforge.unitylibrary/Runtime/Universal/ShaderLibrary/TemporalReprojection/Defines.hlsl"
+
+#include "Packages/eu.rayforge.unitylibrary/Runtime/Core/ShaderLibrary/TemporalReprojection/Params.hlsl"
+#include "Packages/eu.rayforge.unitylibrary/Runtime/Core/ShaderLibrary/TemporalReprojection/TemporalReprojectionImpl.hlsl"
