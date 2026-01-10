c8e12681c4a1097fa5599068c5c2a84641cae3f1
Math <matthias-merkl@gmx.de>
2026-01-10 09:41:23 +0100

    Changelog: shader include fix

--- Runtime/ShaderLibrary/TemporalReprojection/TemporalReprojection.hlsl
+++ Runtime/ShaderLibrary/TemporalReprojection/TemporalReprojection.hlsl
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include "Packages/eu.rayforge.unitylibrary/Runtime/Universal/ShaderLibrary/TemporalReprojection/Defines.hlsl"
+#include "Packages/com.rayforge.urp/Runtime/ShaderLibrary/TemporalReprojection/Defines.hlsl"
 
-#include "Packages/eu.rayforge.unitylibrary/Runtime/Core/ShaderLibrary/TemporalReprojection/Params.hlsl"
-#include "Packages/eu.rayforge.unitylibrary/Runtime/Core/ShaderLibrary/TemporalReprojection/TemporalReprojectionImpl.hlsl"
+#include "Packages/com.rayforge.core/Runtime/ShaderLibrary/TemporalReprojection/Params.hlsl"
+#include "Packages/com.rayforge.core/Runtime/ShaderLibrary/TemporalReprojection/TemporalReprojectionImpl.hlsl"
