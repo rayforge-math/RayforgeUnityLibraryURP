56bc57dde1e4988cbb9863992549d4aa17337bba
Math <matthias-merkl@gmx.de>
2026-01-12 23:09:30 +0100

    Changelog: reprojection defines fixes

--- Runtime/ShaderLibrary/TemporalReprojection/Defines.hlsl
+++ Runtime/ShaderLibrary/TemporalReprojection/Defines.hlsl
@@ -3,16 +3,20 @@
 #define _TAA_MotionVectorTexture        _MotionVectorTexture
 #define sampler_TAA_MotionVectorTexture sampler_MotionVectorTexture
 
-#ifdef _TaaJitter
+#if defined(_TaaJitter)
     #define _TAA_Jitter _TaaJitter
+#elif defined(_TAA_Jitter)
+    #define _TAA_Jitter _TAA_Jitter
 #else
-    #define _TAA_Jitter float2(0.0, 0.0)
+    #define _TAA_Jitter float2(0,0)
 #endif
 
-#ifdef _TaaJitterPrev
+#if defined(_TaaJitterPrev)
     #define _TAA_JitterPrev _TaaJitterPrev
+#elif defined(_TAA_JitterPrev)
+    #define _TAA_JitterPrev _TAA_JitterPrev
 #else
-    #define _TAA_JitterPrev float2(0.0, 0.0)
+    #define _TAA_JitterPrev float2(0,0)
 #endif
 
 #define _TAA_DepthTexture               _CameraDepthTexture
