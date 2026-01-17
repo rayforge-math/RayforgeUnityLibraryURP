f87fcfabe8087e91bce1462da833c02713af0c46
Math <matthias-merkl@gmx.de>
2026-01-17 08:12:23 +0100

    Changelog: change shader includes to relative paths

--- Runtime/ShaderLibrary/TemporalReprojection/TemporalReprojection.hlsl
+++ Runtime/ShaderLibrary/TemporalReprojection/TemporalReprojection.hlsl
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include "Packages/com.rayforge.urp/Runtime/ShaderLibrary/TemporalReprojection/Defines.hlsl"
+#include "./Defines.hlsl"
 
 #include "Packages/com.rayforge.core/Runtime/ShaderLibrary/TemporalReprojection/Params.hlsl"
 #include "Packages/com.rayforge.core/Runtime/ShaderLibrary/TemporalReprojection/TemporalReprojectionImpl.hlsl"
